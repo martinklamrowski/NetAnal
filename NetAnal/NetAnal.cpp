0d0c8a42a17df77ab8ae1475ad5a1b7ee4b8270f
martinklamrowski <25801014+martinklamrowski@users.noreply.github.com>
2019-10-19 17:32:49 -0400

    Started network setup

--- NetAnal/NetAnal.cpp
+++ NetAnal/NetAnal.cpp
@@ -8,22 +8,21 @@ using namespace std;
 
 int main() {
 
-	Node node();
 	string name;
 	int beg;
 	int end;
 	vector<Node> nodes;
 
 	int i = 0;
-	while (i++ < 10) {
-		
+	while (i++ < 3) {
+		cout << "\nEnter da node man: ";
 		cin >> name >> beg >> end;
 		nodes.push_back(Node(name, beg, end));
 	}
 
 	int j = 0;
-	while (j++ < 10) {
-		cout << nodes.at(j).name << endl;
+	while (j < nodes.size()) {
+		cout << nodes.at(j++).name << endl;
 	}
 
 	return 0;
--- NetAnal/Node.cpp
+++ NetAnal/Node.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include "Node.h"
 using namespace std;
 
@@ -6,6 +7,22 @@ Node::Node(string aName, int aEnd, int aBeg) {
 	name = aName;
 	end = aEnd;
 	beg = aBeg;
+	
 	cout << "Node " << name << " created." << endl;
+	cout << "Add adjacent nodes. Enter done when done." << endl;
+	
+	string input;
+	string neighbour;
+	int distance;
+	while (true) {
+		//TODO temporary way of constructing network; does not handle input
+		cout << "Name: ";
+		cin >> neighbour;
+		if (neighbour == "done") break;
+		cout << "Distance from " << name << ": ";
+		cin >> distance;
+		
+		neighbours.emplace_back(neighbour, distance);
+	}
 }
 
